97343e3499bf815ae2213afd9b7985cf9a85ba20
DESKTOP-S9060EB\ASUS <xinia214@gmail.com>
2023-08-06 22:31:51 +0600

    misc changes

---
+++ BracketCombination.js
@@ -0,0 +1,17 @@
+function BracketCombinations(num) {
+
+    function calculatePOssibilities (open, closed) {
+        if (open === 0 && closed === 0)
+            return 1;
+        var res = 0;
+        if (open > 0)
+            res += calculatePOssibilities(open - 1, closed);
+        if (closed > open)
+            res += calculatePOssibilities(open, closed - 1);
+        return res;
+    }
+    // code goes here  
+    return calculatePOssibilities(num, num);
+}
+
+console.log(BracketCombinations(2));
