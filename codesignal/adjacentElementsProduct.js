a09db1f337b45c9bae87fa2cc4732701c07064b4
nusrat <nusratcode14@gmail.com>
2023-08-07 11:45:41 +0600

    misc

---
+++ codesignal/adjacentElementsProduct.js
@@ -0,0 +1,10 @@
+function solution(inputArray) {
+  var max = -Infinity;
+
+  for (var i = 0; i + 1 < inputArray.length; i++) {
+    if (max < inputArray[i] * inputArray[i + 1]) {
+      max = inputArray[i] * inputArray[i + 1];
+    }
+  }
+  return max;
+}
