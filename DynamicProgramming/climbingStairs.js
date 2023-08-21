ae28431325515474e62bf1bd76493f2b832b968b
DESKTOP-S9060EB\ASUS <xinia214@gmail.com>
2023-08-21 22:59:39 +0600

    misc

---
+++ DynamicProgramming/climbingStairs.js
@@ -0,0 +1,19 @@
+var climbStairs = function(n) {
+    if(n <=2){
+        return n;
+    }
+    
+    let prev1 = 1;
+    let prev2 = 2;
+    let current;
+
+    for (let i = 3; i <= n; i++){
+        current = prev1 + prev2;
+        prev1 = prev2;
+        prev2 = current;
+    }
+    return current;
+};
+
+const n = 4;
+console.log(climbStairs(n));
