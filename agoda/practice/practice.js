38633731dffce97fb9c5602b4088e3559e854eff
DESKTOP-S9060EB\ASUS <xinia214@gmail.com>
2023-08-29 23:47:53 +0600

    common problems added

--- agoda/practice/practice.js
+++ agoda/practice/practice.js
@@ -1,41 +1,17 @@
-class Node{
-    constructor(data) {
-        this.data = data;
-        this.next = null;
-    }
-}
+function getElementsByStyle(property, value) {
+    const allElements = document.querySelectorAll('*');
+    const matchingElements = [];
 
-class LinkedList{
-    constructor() {
-        this.head = null;
-    }
-
-    append(data) {
-        const newNode = new Node(data);
-
-        if (!this.head) {
-            this.head = newNode;
-        } else {
-            let current = this.head;
-            while (current.next) {
-                current = current.next;
-            }
-            current.next = newNode
+    allElements.forEach(element => {
+        const computedStyle = window.getComputedStyle(element);
+        if (computedStyle.getPropertyValue(property) === value) {
+            matchingElements.push(element);
         }
-    }
+    });
+
+    return matchingElements;
 }
 
-function arrayToLinkedList(arr) {
-    
-    const linkedList = new LinkedList();
-
-    for (const item of arr) {
-        linkedList.append(item);
-
-    }
-    return linkedList
-}
-
-const array = [1, 2, 3, 4, 5];
-const linkedList = arrayToLinkedList(array);
-console.log(linkedList);
+// Usage example:
+const elementsWithColorRed = getElementsByStyle('color', 'red');
+console.log(elementsWithColorRed);
